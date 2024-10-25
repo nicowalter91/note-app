283a2f633157f242ffbf05715194ca0a80f57aca
Nico Walter <nbierwolf@web.de>
2024-10-25 10:32:39 +0200

    Erstellen der LogIn Seite mit dem Formular

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Colors used in the project
+      colors: {
+        primary: "#2b85ff",
+        secondary: "#ef863e",
+      },
+    },
   },
   plugins: [],
 }
