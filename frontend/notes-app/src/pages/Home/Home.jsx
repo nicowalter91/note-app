68131c8a40cf39ab94d6133573bba3100516c9d7
Nico Walter <nbierwolf@web.de>
2024-10-29 14:35:17 +0100

    neueste Updates

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -32,17 +32,33 @@ const Home = () => {
           </div>    
       </div>
 
-      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={()=> {}}>
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' 
+      onClick={()=> {
+        setOpenAddEditModal({ isShown: true, type: "add", data: null});
+      }}>
         <MdAdd className="text-[32px] text-white" />
       </button>
 
-       <Modal isOpen={openAddEditModal.isShown} onRequestClose={() => {}} style={{overlay: {backgroundColor: "rgba(0,0,0,0.2)",},}}
-       contentLabel=""
-       className=""
-       >
+       <Modal 
+       isOpen={openAddEditModal.isShown} 
+       onRequestClose={() => {}} 
+       style={{
+        overlay: {
+          backgroundColor: "rgba(0,0,0,0.2)",
+        },
+      }}
+      contentLabel=""
+      className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
       
 
-      <AddEditNotes />
+      <AddEditNotes 
+      type={openAddEditModal.type}
+        noteData={openAddEditModal.data}
+        onClose={() => {
+          setOpenAddEditModal({ isShown: false, type: "add", data: null});
+        }}
+      />
       </Modal>
       </>
     </div>
