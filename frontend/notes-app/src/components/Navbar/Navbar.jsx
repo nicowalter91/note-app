88a5b6ebc60d2733bbd5f3eccb27a10e15f7447b
Nico Walter <nbierwolf@web.de>
2024-11-30 17:49:13 +0100

    fixed: Logout in Profilinfo

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import SearchBar from '../SearchBar/SearchBar'; // Importiert die SearchBar-Komponente
 import ProfileInfo from '../Cards/ProfileInfo'; // Importiert die ProfileInfo-Komponente
 import Logo from '../../assets/img/Logo.png'; // Importiert das Logo
 
-const Navbar = ({ userInfo, onSearchNote, handleClearSearch, searchQuery, setSearchQuery }) => {
+const Navbar = ({ userInfo, onLogout }) => {
+
+  
   return (
     <div className="flex items-center justify-between px-6 py-2 bg-white shadow-md">
       <div className="flex items-center">
@@ -20,7 +21,7 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch, searchQuery, setSea
 
 
       {/* Anzeige der Benutzerdaten und Logout-Button */}
-      <ProfileInfo userInfo={userInfo} />
+      <ProfileInfo userInfo={userInfo} onLogout={onLogout}/>
     </div>
   );
 };
